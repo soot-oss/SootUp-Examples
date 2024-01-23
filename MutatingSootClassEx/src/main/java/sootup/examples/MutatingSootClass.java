b91f72eb8cdfa2eead370d992dbaea570961a033
Reshma Sobha Nair <100214326+ReshmaSobhaNair@users.noreply.github.com>
2024-01-23 21:19:07 +0100

    Commit to the mutating sootclass

--- MutatingSootClassEx/src/main/java/sootup/examples/MutatingSootClass.java
+++ MutatingSootClassEx/src/main/java/sootup/examples/MutatingSootClass.java
@@ -7,17 +7,18 @@ import sootup.core.inputlocation.AnalysisInputLocation;
 import sootup.core.jimple.basic.Local;
 import sootup.core.model.Body;
 import sootup.core.model.SootClass;
-import sootup.core.model.SootMethod;
 import sootup.core.signatures.MethodSignature;
-import sootup.core.types.ClassType;
+import sootup.core.signatures.MethodSubSignature;
+import sootup.core.signatures.PackageName;
+import sootup.core.types.ArrayType;
 import sootup.core.types.PrimitiveType.IntType;
+import sootup.core.types.VoidType;
 import sootup.java.bytecode.inputlocation.PathBasedAnalysisInputLocation;
-import sootup.java.core.JavaProject;
 import sootup.java.core.JavaSootClass;
-import sootup.java.core.JavaSootClassSource;
+import sootup.java.core.JavaSootMethod;
 import sootup.java.core.OverridingJavaClassSource;
 import sootup.java.core.language.JavaJimple;
-import sootup.java.core.language.JavaLanguage;
+import sootup.java.core.types.JavaClassType;
 import sootup.java.core.views.JavaView;
 
 /**
@@ -29,71 +30,70 @@ public class MutatingSootClass {
   public static void main(String[] args) {
     // Create a AnalysisInputLocation, which points to a directory. All class files will be loaded
     // from the directory
-    AnalysisInputLocation<JavaSootClass> inputLocation =
+    AnalysisInputLocation inputLocation =
         PathBasedAnalysisInputLocation.create(
             Paths.get("src/test/resources/Mutatingclasssoot/binary"), null);
 
-    // Specify the language of the JavaProject. This is especially relevant for Multi-release jars,
-    // where classes are loaded depending on the language level of the analysis
-    JavaLanguage language = new JavaLanguage(8);
-
-    // Create a new JavaProject based on the input location
-    JavaProject project = JavaProject.builder(language).addInputLocation(inputLocation).build();
+    // Create a view for project, which allows us to retrieve classes
+    JavaView view = new JavaView(inputLocation);
 
     // Create a signature for the class we want to analyze
-    ClassType classType = project.getIdentifierFactory().getClassType("HelloWorld");
+    JavaClassType classType = view.getIdentifierFactory().getClassType("HelloWorld");
 
     // Create a signature for the method we want to analyze
     MethodSignature methodSignature =
-        project
-            .getIdentifierFactory()
+        view.getIdentifierFactory()
             .getMethodSignature(
                 classType, "main", "void", Collections.singletonList("java.lang.String[]"));
 
-    // Create a view for project, which allows us to retrieve classes
-    JavaView view = project.createView();
+    // Check that class is present
+    if (!view.getClass(classType).isPresent()) {
+      System.out.println("Class not found.");
+      return;
+    }
 
     // Retrieve class
-    SootClass<JavaSootClassSource> sootClass = view.getClass(classType).get();
+    JavaSootClass sootClass = view.getClass(classType).get();
 
-    // Retrieve the specified method from the class.
-    SootMethod method = view.getMethod(methodSignature).get();
+    // Check and retrieve method
+    if (!view.getMethod(methodSignature).isPresent()) {
+      System.out.println("Method not found.");
+      return;
+    }
+    JavaSootMethod method = view.getMethod(methodSignature).get();
     Body oldBody = method.getBody();
 
-    // Print the original method body.
     System.out.println(oldBody);
-
-    // Create OverridingBodySource
-    //    OverridingBodySource overridingBodySource =
-    //            new OverridingBodySource(methodSignature, method.getBody());
-
-    // Local variables and method body modifications are commented out.
-
-    // Create a local variable of type 'int'.
     Local newLocal = JavaJimple.newLocal("helloWorldLocal", IntType.getInt());
 
-    // Create a new method body with the added local variable.
     Body newBody = oldBody.withLocals(Collections.singleton(newLocal));
 
-    // Modify the method body source with the new body.
     OverridingBodySource newBodySource =
         new OverridingBodySource(method.getBodySource()).withBody(newBody);
 
-    // Create an overriding class source for the retrieved class
     OverridingJavaClassSource overridingJavaClassSource =
         new OverridingJavaClassSource(sootClass.getClassSource());
 
-    // Create a new method with the modified body.
-    SootMethod newMethod = method.withOverridingMethodSource(old -> newBodySource);
+    JavaSootMethod newMethod = method.withOverridingMethodSource(old -> newBodySource);
 
-    // Replace the original method in the class source with the new method.
     OverridingJavaClassSource newClassSource =
         overridingJavaClassSource.withReplacedMethod(method, newMethod);
+    SootClass newClass = sootClass.withClassSource(newClassSource);
 
-    // Create a new class with the modified class source.
-    SootClass<JavaSootClassSource> newClass = sootClass.withClassSource(newClassSource);
-
-    // Print the body of the first method in the modified class.
     System.out.println(newClass.getMethods().stream().findFirst().get().getBody());
+
+    if (newClass
+        .getMethod(
+            new MethodSubSignature(
+                "main",
+                Collections.singletonList(
+                    new ArrayType(new JavaClassType("String", new PackageName("java.lang")), 1)),
+                VoidType.getInstance()))
+        .get().getBody().getLocals().stream()
+        .anyMatch(local -> local.equals(newLocal))) {
+      System.out.println("New local exists in the modified method.");
+    } else {
+      System.out.println("New local does not exist in the modified method.");
+    }
   }
 }
