1626f9c6b42da74dc3a6c83d51e8af01cdbb8cb5
Reshma Sobha Nair <100214326+ReshmaSobhaNair@users.noreply.github.com>
2024-02-08 00:01:07 +0100

    BasicSetup1.1.2

---
+++ BasicSetupExample1.1.2/src/main/java/sootup/examples/BasicSetup.java
@@ -0,0 +1,103 @@
+package sootup.examples;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import sootup.core.Project;
+import sootup.core.inputlocation.AnalysisInputLocation;
+import sootup.core.jimple.common.expr.JVirtualInvokeExpr;
+import sootup.core.jimple.common.stmt.JInvokeStmt;
+import sootup.core.model.SootClass;
+import sootup.core.model.SootMethod;
+import sootup.core.signatures.MethodSignature;
+import sootup.core.types.ClassType;
+import sootup.core.views.View;
+import sootup.java.bytecode.inputlocation.JavaClassPathAnalysisInputLocation;
+import sootup.java.bytecode.inputlocation.PathBasedAnalysisInputLocation;
+import sootup.java.core.JavaProject;
+import sootup.java.core.JavaSootClass;
+import sootup.java.core.language.JavaJimple;
+import sootup.java.core.language.JavaLanguage;
+import sootup.java.sourcecode.inputlocation.JavaSourcePathAnalysisInputLocation;
+import sootup.jimple.parser.JimpleAnalysisInputLocation;
+
+/** This example illustrates how to create and use a new Soot Project. */
+public class BasicSetup {
+
+  public static void main(String[] args) {
+    // Create a AnalysisInputLocation, which points to a directory. All class files will be loaded
+    // from the directory
+    Path pathToBinary = Paths.get("src/test/resources/Basicsetup/binary");
+    // AnalysisInputLocation inputLocation = PathBasedAnalysisInputLocation.create(pathToBinary,
+    // null);
+
+
+    AnalysisInputLocation<JavaSootClass> inputLocation =
+            new PathBasedAnalysisInputLocation(pathToBinary,null);
+
+    JavaLanguage language = new JavaLanguage(8);
+
+    Project project = JavaProject.builder(language).addInputLocation(inputLocation).build();
+    // Create a view for project, which allows us to retrieve classes
+    View view = project.createView();
+
+
+
+
+
+
+
+
+    // Create a signature for the class we want to analyze
+    ClassType classType = project.getIdentifierFactory().getClassType("HelloWorld");
+
+
+    // Create a signature for the method we want to analyze
+    MethodSignature methodSignature =
+            view.getIdentifierFactory()
+                    .getMethodSignature(
+                            classType, "main", "void", Collections.singletonList("java.lang.String[]"));
+
+    // Check if the class "HelloWorld" is present in the project.
+    if (!view.getClass(classType).isPresent()) {
+      System.out.println("Class not found!");
+      return;
+    }
+
+    // Retrieve the specified class from the project.
+    SootClass sootClass = (SootClass) view.getClass(classType).get();
+
+    // Retrieve method
+    view.getMethod(methodSignature);
+
+    if (!sootClass.getMethod(methodSignature.getSubSignature()).isPresent()) {
+      System.out.println("Method not found!");
+      return; // Exit if the method is not found
+    }
+
+    // Retrieve the specified method from the class.
+    SootMethod sootMethod =
+        (SootMethod) sootClass.getMethod(methodSignature.getSubSignature()).get();
+
+    // Read jimple code of method
+    System.out.println(sootMethod.getBody());
+
+    // Check if the method contains a specific print statement ("Hello World!").
+    boolean helloWorldPrintPresent =
+        sootMethod.getBody().getStmts().stream()
+            .anyMatch(
+                stmt ->
+                    stmt instanceof JInvokeStmt
+                        && stmt.getInvokeExpr() instanceof JVirtualInvokeExpr
+                        && stmt.getInvokeExpr()
+                            .getArg(0)
+                            .equivTo(JavaJimple.getInstance().newStringConstant("Hello World!")));
+
+    // Print the result of the check.
+    if (helloWorldPrintPresent) {
+      System.out.println("Hello World print is present.");
+    } else {
+      System.out.println("Hello World print is not present.");
+    }
+  }
+}
