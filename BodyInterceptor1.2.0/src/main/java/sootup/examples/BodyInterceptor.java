e5c8a956c701637f0906a6c98ac8c5137baadbb9
Reshma Sobha Nair <100214326+ReshmaSobhaNair@users.noreply.github.com>
2024-02-15 15:10:10 +0100

    Commit to 1.2.0 version of example projects

---
+++ BodyInterceptor1.2.0/src/main/java/sootup/examples/BodyInterceptor.java
@@ -0,0 +1,69 @@
+package sootup.examples;
+
+import java.util.Collections;
+import sootup.core.inputlocation.AnalysisInputLocation;
+import sootup.core.jimple.common.constant.IntConstant;
+import sootup.core.jimple.common.stmt.JAssignStmt;
+import sootup.core.model.SootClass;
+import sootup.core.model.SootMethod;
+import sootup.core.signatures.MethodSignature;
+import sootup.core.types.ClassType;
+import sootup.java.bytecode.inputlocation.JavaClassPathAnalysisInputLocation;
+import sootup.java.bytecode.interceptors.DeadAssignmentEliminator;
+import sootup.java.core.views.JavaView;
+
+/** This example illustrates how to invoke body interceptors. */
+public class BodyInterceptor {
+
+  public static void main(String[] args) {
+    // Create a AnalysisInputLocation, which points to a directory. All class files will be loaded
+    // from the directory
+    AnalysisInputLocation inputLocation =
+        new JavaClassPathAnalysisInputLocation(
+            "src/test/resources/BodyInterceptor/binary",
+            null,
+            Collections.singletonList(new DeadAssignmentEliminator()));
+
+    // Create a new JavaView based on the input location
+    JavaView view = new JavaView(inputLocation);
+
+    // Create a signature for the class we want to analyze
+    ClassType classType = view.getIdentifierFactory().getClassType("File");
+
+    // Create a signature for the method we want to analyze
+    MethodSignature methodSignature =
+        view.getIdentifierFactory()
+            .getMethodSignature(classType, "someMethod", "void", Collections.emptyList());
+
+    // Check if class is present
+    if (!view.getClass(classType).isPresent()) {
+      System.out.println("Class not found.");
+      return;
+    }
+
+    SootClass sootClass = view.getClass(classType).get();
+
+    // Retrieve method
+    if (!view.getMethod(methodSignature).isPresent()) {
+      System.out.println("Method not found.");
+      return;
+    }
+    SootMethod method = view.getMethod(methodSignature).get();
+
+    System.out.println(method.getBody());
+
+    // Check if l1 = 3 is not present, i.e., body interceptor worked
+    boolean interceptorWorked =
+        method.getBody().getStmts().stream()
+            .noneMatch(
+                stmt ->
+                    stmt instanceof JAssignStmt
+                        && ((JAssignStmt) stmt).getRightOp().equivTo(IntConstant.getInstance(3)));
+
+    if (interceptorWorked) {
+      System.out.println("Interceptor worked as expected.");
+    } else {
+      System.out.println("Interceptor did not work as expected.");
+    }
+  }
+}
---
+++ CallgraphExample1.2.0/src/main/java/sootup/examples/CallgraphExample.java
@@ -0,0 +1,59 @@
+package sootup.examples;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import sootup.callgraph.CallGraph;
+import sootup.callgraph.CallGraphAlgorithm;
+import sootup.callgraph.ClassHierarchyAnalysisAlgorithm;
+import sootup.core.inputlocation.AnalysisInputLocation;
+import sootup.core.signatures.MethodSignature;
+import sootup.core.typehierarchy.ViewTypeHierarchy;
+import sootup.core.types.ClassType;
+import sootup.core.types.VoidType;
+import sootup.java.bytecode.inputlocation.JavaClassPathAnalysisInputLocation;
+import sootup.java.core.JavaIdentifierFactory;
+//import sootup.java.core.JavaProject;
+import sootup.java.core.JavaSootClass;
+import sootup.java.core.language.JavaLanguage;
+import sootup.java.core.views.JavaView;
+
+public class CallgraphExample {
+
+    public static void main(String[] args) {
+        // Create a AnalysisInputLocation, which points to a directory. All class files will be loaded
+        // from the directory
+        List<AnalysisInputLocation> inputLocations = new ArrayList<>();
+        inputLocations.add(
+                new JavaClassPathAnalysisInputLocation("src/test/resources/Callgraph/binary"));
+        inputLocations.add(
+                new JavaClassPathAnalysisInputLocation(
+                        System.getProperty("java.home") + "/lib/rt.jar")); // add rt.jar
+
+        JavaView view = new JavaView(inputLocations);
+
+        // Get a MethodSignature
+        ClassType classTypeA = view.getIdentifierFactory().getClassType("A");
+        ClassType classTypeB = view.getIdentifierFactory().getClassType("B");
+        MethodSignature entryMethodSignature =
+                JavaIdentifierFactory.getInstance()
+                        .getMethodSignature(
+                                classTypeB,
+                                JavaIdentifierFactory.getInstance()
+                                        .getMethodSubSignature(
+                                                "calc", VoidType.getInstance(), Collections.singletonList(classTypeA)));
+
+        // Create type hierarchy and CHA
+        final ViewTypeHierarchy typeHierarchy = new ViewTypeHierarchy(view);
+        System.out.println(typeHierarchy.subclassesOf(classTypeA));
+        CallGraphAlgorithm cha = new ClassHierarchyAnalysisAlgorithm(view);
+
+        // Create CG by initializing CHA with entry method(s)
+        CallGraph cg = cha.initialize(Collections.singletonList(entryMethodSignature));
+
+        cg.callsFrom(entryMethodSignature).forEach(System.out::println);
+    }
+
+
+}
