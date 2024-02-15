2ce1f587c02870958ccef23abedbcad50e6bbed9
Reshma_Sobha_Nair <100214326+ReshmaSobhaNair@users.noreply.github.com>
2024-02-15 15:23:30 +0100

    Delete Callgraph directory

--- Callgraph/target/test-classes/Callgraph/source/B.java
+++
@@ -1,5 +0,0 @@
-public class B {
-  public void calc(A a) {
-    a.calc(1); // can be A.calc, B.calc, C.calc
-  }
-}
--- Callgraph/target/test-classes/Callgraph/source/C.java
+++
@@ -1,5 +0,0 @@
-public class C extends A {
-  public int calc(int a) {
-    return a + 2;
-  }
-}
